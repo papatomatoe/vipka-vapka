df39ee3d5570acfc7c9fc6336728114282e2db74
papatomatoe <papatomatoe.project@gmail.com>
2023-01-15 19:41:39 +0300

    add mobile and tablet versions

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -5,23 +5,23 @@
 <footer class="footer">
 	<div class="footer__wrapper wrapper">
 		<a class="footer__logo" href="/"><img src={logo} alt="Выпка Вапка" /></a>
-		<ul class="header__menu">
-			<li class="header__item">
+		<ul class="footer__menu">
+			<li class="footer__item">
 				<a href="/">на главную</a>
 			</li>
-			<li class="header__item">
+			<li class="footer__item">
 				<a href="/blog">почитать</a>
 			</li>
-			<li class="header__item header__item--left">
+			<li class="footer__item footer__item--left">
 				<a href="/store">купить</a>
 			</li>
-			<li class="header__item header__item--right">
+			<li class="footer__item footer__item--right">
 				<a href="/about-me">обо мне</a>
 			</li>
-			<li class="header__item">
+			<li class="footer__item">
 				<a href="/contacts">контакты</a>
 			</li>
-			<li class="header__item">
+			<li class="footer__item">
 				<a href="/secret">секрет</a>
 			</li>
 		</ul>
@@ -31,11 +31,11 @@
 
 <style>
 	.footer {
-		--height-element: 80px;
-
+		--height-element: 30px;
+		height: 100px;
 		position: relative;
 		background-color: var(--color-bg-footer);
-		padding: var(--height-element) 0 51px;
+		padding: var(--height-element) 0 30px;
 	}
 
 	.footer::before {
@@ -50,39 +50,84 @@
 
 	.footer__wrapper {
 		width: 100%;
-		display: grid;
-		grid-template-columns: min-content 1fr min-content;
-		align-items: end;
 	}
 
 	.footer__logo {
-		position: relative;
-		width: 92px;
-		top: 10px;
+		display: none;
 	}
 
-	.header__menu {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		gap: 31px;
-	}
-
-	.header__item a {
-		font-family: var(--font-accent);
-		font-weight: 700;
-		font-size: 16px;
-		line-height: 1;
-		color: var(--color-text);
+	.footer__menu {
+		display: none;
 	}
 	.footer__copy {
 		position: relative;
-		top: -4px;
+		text-align: center;
+		top: 20px;
 		min-width: 151px;
 		font-family: var(--font-accent);
 		font-weight: 700;
 		font-size: 16px;
 		line-height: 1;
 		color: var(--color-text);
+	}
+
+	@media (min-width: 768px) {
+		.footer {
+			--height-element: 70px;
+			height: 200px;
+		}
+		.footer__wrapper {
+			width: 100%;
+			display: grid;
+			grid-template-columns: min-content 1fr min-content;
+			grid-template-columns: 1fr 1fr;
+			align-items: end;
+		}
+		.footer__logo {
+			display: block;
+			position: relative;
+			width: 92px;
+			top: 10px;
+		}
+		.footer__item a {
+			font-family: var(--font-accent);
+			font-weight: 700;
+			font-size: 16px;
+			line-height: 1;
+			color: var(--color-text);
+		}
+		.footer__copy {
+			text-align: right;
+			top: 0;
+		}
+	}
+
+	@media (min-width: 1110px) {
+		.footer {
+			--height-element: 80px;
+			height: 200px;
+		}
+		.footer__wrapper {
+			margin-top: 20px;
+			grid-template-columns: min-content 1fr min-content;
+		}
+		.footer__menu {
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			gap: 31px;
+			margin-left: 40px;
+		}
+
+		.footer__copy {
+			position: relative;
+			top: -4px;
+			min-width: 151px;
+			font-family: var(--font-accent);
+			font-weight: 700;
+			font-size: 16px;
+			line-height: 1;
+			color: var(--color-text);
+		}
 	}
 </style>
--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,40 +1,85 @@
 <script lang="ts">
+	import { slide } from 'svelte/transition';
+	import { sineOut } from 'svelte/easing';
 	import logo from '$lib/assets/logo.svg';
+
+	import { windowWidth } from '$lib/stores/environment';
+
+	let opened = false;
+
+	const handleToggleMenu = () => {
+		const body = document.querySelector('body');
+
+		body?.classList.contains('block') && opened
+			? body.classList.remove('block')
+			: body?.classList.add('block');
+
+		opened = !opened;
+	};
+
+	$: isMobile = $windowWidth > 768;
 </script>
 
-<header class="header">
-	<nav class="header__nav wrapper">
-		<a href="/" class="header__logo"><img src={logo} alt="Вапка Выпка" /></a>
-		<ul class="header__menu">
-			<li class="header__item header__item--left">
-				<a href="/">на главную</a>
-			</li>
-			<li class="header__item header__item--left">
-				<a href="/blog">почитать</a>
-			</li>
-			<li class="header__item header__item--left">
-				<a href="/store">купить</a>
-			</li>
-			<li class="header__item header__item--right">
-				<a href="/about-me">обо мне</a>
-			</li>
-			<li class="header__item header__item--right">
-				<a href="/contacts">контакты</a>
-			</li>
-			<li class="header__item header__item--right">
-				<a href="/secret">секрет</a>
-			</li>
-		</ul>
+<svelte:window bind:innerWidth={$windowWidth} />
+
+<header class="header" class:header--show-menu={opened}>
+	<nav class="header__nav wrapper" class:header__nav--opened={opened}>
+		<a href="/" class="header__logo"><img src={logo} alt="Вапка Выпка" width="150" /></a>
+		{#if opened || isMobile}
+			<ul transition:slide={{ duration: 300, easing: sineOut }} class="header__menu" class:opened>
+				<li class="header__item header__item--left">
+					<a href="/">на главную</a>
+				</li>
+				<li class="header__item header__item--left">
+					<a href="/blog">почитать</a>
+				</li>
+				<li class="header__item header__item--left">
+					<a href="/store">купить</a>
+				</li>
+				<li class="header__item header__item--right">
+					<a href="/about-me">обо мне</a>
+				</li>
+				<li class="header__item header__item--right">
+					<a href="/contacts">контакты</a>
+				</li>
+				<li class="header__item header__item--right">
+					<a href="/secret">секрет</a>
+				</li>
+			</ul>
+		{/if}
 	</nav>
+	<button
+		class="header__open-menu"
+		class:header__open-menu--opened={opened}
+		on:click={handleToggleMenu}
+		><svg xmlns="http://www.w3.org/2000/svg" width="34" height="17" fill="none"
+			><path
+				stroke="#000"
+				stroke-width="3"
+				d="m1 12 16 2.5L33 12M1 6.68l16 2.5 16-2.5m-32-5 16 2.5 16-2.5"
+			/></svg
+		></button
+	>
 </header>
 
 <style>
 	.header {
-		--height-element: 80px;
+		--height-element: 30px;
+
+		display: grid;
+		grid-template-rows: 1fr min-content;
+		gap: 15px;
 		position: relative;
 		background-color: var(--color-bg-header);
-		padding: 80px 0 0;
+		padding: 20px 0 30px;
 		margin-bottom: var(--height-element);
+		height: 120px;
+
+		transition: all 0.3s ease-out;
+	}
+
+	.header__nav--opened {
+		overflow-y: auto;
 	}
 
 	.header::after {
@@ -47,32 +92,43 @@
 		top: 100%;
 	}
 
-	.header__logo {
-		position: absolute;
-		top: 25px;
-		left: calc(50% + 10px);
-		transform: translateX(-50%);
+	.header--show-menu {
+		height: 100vh;
+	}
+
+	.header__logo img {
+		display: block;
+		width: 150px;
+		margin: 0 auto;
+	}
+
+	.header__open-menu {
+		left: 50%;
+		background-color: transparent;
+		border: none;
+		margin: 0 auto;
+		padding: 0;
+		width: 40px;
+		height: 40px;
+		display: grid;
+		place-items: center;
 		z-index: 1;
+		transition: transform 0.3s ease-out;
+	}
+
+	.header__open-menu--opened {
+		transform: rotateX(180deg);
 	}
 
 	.header__menu {
-		display: flex;
+		display: none;
 	}
 
-	.header__item--left {
-		margin-right: 60px;
-	}
-
-	.header__item--right {
-		margin-left: 60px;
-	}
-
-	.header__item:nth-of-type(3) {
-		margin-right: auto;
-	}
-
-	.header__item:nth-of-type(4) {
-		margin-left: auto;
+	.opened {
+		display: grid;
+		place-items: center;
+		padding: 45px 0;
+		margin: auto;
 	}
 
 	.header__item a {
@@ -80,5 +136,71 @@
 		color: var(--color-text);
 		font-size: 36px;
 		font-weight: 700;
+	}
+
+	@media (min-width: 768px) {
+		.header {
+			--height-element: 70px;
+			height: 240px;
+			padding: 35px 0 0;
+		}
+
+		.header__nav {
+			z-index: 1;
+		}
+
+		.header__logo img {
+			width: 175px;
+		}
+
+		.header__menu {
+			margin-top: 50px;
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+		}
+
+		.header__open-menu {
+			display: none;
+		}
+	}
+	@media (min-width: 1110px) {
+		.header {
+			--height-element: 80px;
+			height: auto;
+			padding: 80px 0 0;
+		}
+
+		.header__logo {
+			position: absolute;
+			top: 25px;
+			left: calc(50% + 10px);
+			transform: translateX(-50%);
+			z-index: 1;
+		}
+		.header__logo img {
+			width: 255px;
+		}
+
+		.header__menu {
+			display: flex;
+			margin: 0;
+		}
+
+		.header__item--left {
+			margin-right: 60px;
+		}
+
+		.header__item--right {
+			margin-left: 60px;
+		}
+
+		.header__item:nth-of-type(3) {
+			margin-right: auto;
+		}
+
+		.header__item:nth-of-type(4) {
+			margin-left: auto;
+		}
 	}
 </style>
